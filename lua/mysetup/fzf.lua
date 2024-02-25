40696a26787b7ca03e939e55b2eea66bb06e2aed
Daniel Gustavo <dg520589@gmail.com>
2024-02-25 13:12:19 -0300

    fix: typo

--- lua/mysetup/fzf.lua
+++ lua/mysetup/fzf.lua
@@ -1,5 +1,5 @@
 nmap("<leader>f", ":Telescope find_files<CR>") -- show files
 nmap("<leader>r", ":Telescope live_grep<CR>") -- open ripgrep
-nmap("<leader>rg", ":Telescope live_grep<<CR>") -- open ripgrep
+nmap("<leader>rg", ":Telescope live_grep<CR>") -- open ripgrep
 nmap("<leader>ll", ":BLines<CR>") -- search string in file
 nmap("<leader>l", ":BLines<CR>") -- search string in file
